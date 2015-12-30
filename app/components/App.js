170c1bbb0c054ac643f062cefbd7bc8b0f78387c
Ed Gauci <ed@gauci.co>
2015-12-30 14:44:48 -0800

    Note app

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,57 @@
-import React from 'react';
-import Note from './Note.js';
+import uuid from 'node-uuid';
+import React, {Component} from 'react';
+import Notes from './Notes.js';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do Laundry'
+        }
+      ]
+    };
+  }
   render() {
-    return <Note />;
+    const notes = this.state.notes;
+    return (
+      <div>
+        <button className="add-note" onClick={this.addNote}>+</button>
+        <Notes notes={notes} onEdit={this.editNote} onDelete={this.deleteNote} />
+      </div>
+    );
+  }
+  addNote = () => {
+    /* eslint no-invalid-this: 0 */
+    this.setState({
+      notes: [...this.state.notes, {
+        id: uuid.v4(),
+        task: 'New Task'
+      }]
+    });
+  }
+  editNote = (id, task) => {
+    const notes = this.state.notes.map((note) => {
+      if (note.id === id) {
+        note.task = task;
+      }
+      return note;
+    });
+    this.setState({notes});
+  }
+  deleteNote = (id) => {
+    this.setState({
+      notes: this.state.notes.filter((note) => note.id !== id)
+    });
   }
 }
--- app/components/Note.js
+++ app/components/Note.js
@@ -1,3 +1,64 @@
-import React from 'react';
+/* eslint no-invalid-this: 0 */
+import React, {Component, PropTypes} from 'react';
 
-export default () => <div>Learn Webpack Now!</div>;
+export default class Note extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false
+    };
+  }
+
+  static propTypes = {
+    task: PropTypes.string.isRequired,
+    onEdit: PropTypes.func,
+    onDelete: PropTypes.func
+  }
+
+  render() {
+    if (this.state.editing) {
+      return this.renderEdit();
+    }
+    return this.renderNote();
+  }
+
+  renderEdit = () => (
+    <input type="text"
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />
+  )
+
+  renderNote = () => (
+    <div onClick={this.edit}>
+      <span className="task">{this.props.task}</span>
+      {this.props.onDelete ? this.renderDelete() : null}
+    </div>
+  )
+
+  renderDelete = () => (
+    <button className="delete" onClick={this.props.onDelete}>x</button>
+  )
+
+  edit = () => {
+    this.setState({
+      editing: true
+    });
+  }
+
+  checkEnter = (e) => {
+    if (e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  }
+
+  finishEdit = (e) => {
+    if (this.props.onEdit) {
+      this.props.onEdit(e.target.value);
+    }
+    this.setState({
+      editing: false
+    });
+  }
+}
